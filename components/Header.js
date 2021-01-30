56a962c04d6f45b7e9e4119f10973e2ddd8de716
Matt <gouldie70@gmail.com>
2021-01-30 15:54:07 +0000

    clientOnly HOC

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import { PageWithHeader, TopNav, useBreakpoint } from 'bumbag'
 import HamburgerIcon from '../components/icons/Hamburger'
 import ColorModeIcon from '../components/icons/ColorMode'
+import ClientOnly from '../utils/clientOnly'
 
 export default function Header ({ children }) {
   const isDesktop = useBreakpoint('min-widescreen')
@@ -12,11 +13,12 @@ export default function Header ({ children }) {
           <TopNav.Item href='/'>Collector</TopNav.Item>
         </TopNav.Section>
         <TopNav.Section marginRight='major-4'>
-        <TopNav.Item>
-          {isDesktop && <ColorModeIcon />}
-          {!isDesktop && <HamburgerIcon />}
-        </TopNav.Item>
-
+        <ClientOnly>
+          <TopNav.Item>
+            {isDesktop && <ColorModeIcon />}
+            {!isDesktop && <HamburgerIcon />}
+          </TopNav.Item>
+        </ClientOnly>
         </TopNav.Section>
       </TopNav>
     }>
