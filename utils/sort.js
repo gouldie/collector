34ccec495e315f1dadd97e7323c60e9a3436d045
Matt <gouldie70@gmail.com>
2021-02-01 18:43:46 +0000

    sort cards

--- utils/sort.js
+++ utils/sort.js
@@ -1,4 +1,4 @@
 export default {
-  no: (a, b) => a.no < b.no ? 1 : -1,
-  name: (a, b) => a.name < b.name ? 1 : -1
+  no: (a, b) => a.no < b.no ? -1 : 1,
+  name: (a, b) => a.name < b.name ? -1 : 1
 }
