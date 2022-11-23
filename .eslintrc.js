9132d2acecd737de233e3d41876c24b529d3043b
matthew.gould <matthew.gould@morneaushepell.com>
2022-11-23 20:28:52 +0000

    added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -14,9 +11,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'jsx-quotes': ['error', 'prefer-single'],
